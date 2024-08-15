64ee0239c53367566e2c646faff3cfb2ce3a0a94
Girish keleng <girishkeleng30@gmail.com>
2024-08-15 13:41:54 +0530

    checkOrderForHostClient1

--- client/src/components/miniComponents/OrderInboxSm.jsx
+++ client/src/components/miniComponents/OrderInboxSm.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import HeaderSm from "./HeaderSm";
 import Sidebar from "./Sidebar";
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import { Link } from "react-router-dom";
 
 export default function OrderInboxSm({ placeId, showOrder }) {
 
@@ -29,25 +30,42 @@ export default function OrderInboxSm({ placeId, showOrder }) {
 
     return (
         <div>
-            <div className=" "  >
+            <div className=""  >
                 <HeaderSm />
                 <div className="flex flex-1 ">
                     <Sidebar />
 
                     <div className="relative">
-                        <div className="absolute top-4 left-4" onClick={() => showOrder(false)}>
+                        <div className="absolute top-1 left-2" onClick={() => showOrder(false)}>
                             <ArrowBackIosNewIcon />
                         </div>
                     </div>
-                    <div className="w-10/12 mx-auto bg-purple-100">
-                        
-                     {orderData.length && orderData.length>0 && orderData.map((item,inx)=>(
-                        <div key={inx}>
+                    <div className="w-9/12 mx-auto ">
+                        <div className="grid grid-cols-1 lg:grid-cols-4 gap-12 mt-4">
 
-                            <p>{item.name}</p>
+                           
+
+                            {orderData && orderData.length > 0 && orderData.map((item, inx) => (
+                                <div key={inx} className="bg-slate-50 py-2 px-4 rounded-md">
+
+                                    <Link to={`/UserProfile/${item.user_id}`} className="text-lg">Booked by {item.name}</Link>
+                                    <p>Contact No:  {item.phone_no} </p>
+                                    <p> Email:  {item.email} </p>
+                                    <p> payment_Id :  {item.razorpay_payment_id} </p>
+                                    <p> Order_Id :  {item.razorpay_order_id} </p>
+                                    <p></p>
+
+
+
+                                </div>
+                            ))}
+                             {orderData.length === 0 && (
+                                <div className="text-center text-2xl col-span-4 bg-red-500">
+                                    No orders found for this place
+                                </div>
+                            )}
+
                         </div>
-                     ))}
-
                     </div>
 
 
