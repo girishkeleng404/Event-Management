94e3ec3d539f5ef469660dda81869f747d0a2c45
Girish keleng <girishkeleng30@gmail.com>
2024-09-03 22:24:40 +0530

    refactor: Update login route to use passport authentication

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
 import {login, register} from "../controllers/authController.js";
+import passport from "passport";
 const router = express.Router();
 
 
 router.route('/register').post(register);
-router.route('/login').post(login)
+router.route('/login').post(passport.authenticate('local'),login);
 
 export default router;
