46201bf9b7ec27ffaa88a63977a3ce2f7e0b1049
Girish keleng <girishkeleng30@gmail.com>
2024-09-05 20:07:29 +0530

    refactor: Move crypto import to RazorpayController.js

--- api/controllers/RazorpayController.js
+++ api/controllers/RazorpayController.js
@@ -1,5 +1,6 @@
 
 import Razorpay from "razorpay";
+import crypto from "crypto";
 
 const orders = async(req,res)=>{
     const razorpay = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_KEY_SECRET });
--- api/index.js
+++ api/index.js
@@ -14,9 +14,8 @@ import OpenIDConnectStrategy from "passport-openidconnect";
 import helmet from "helmet";
 import jwt from "jsonwebtoken";
 import env from "dotenv"
-import razorpay from "razorpay";
-import Razorpay from "razorpay";
-import crypto from "crypto";
+
+
 
 
 import authRoute from './routes/authRoute.js'
@@ -185,68 +184,6 @@ app.use(ISFS_Route);
 
 
 app.use(RazorpayRoute);
-
-// app.post('/orders', async (req, res) => {
-//     const razorpay = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_KEY_SECRET });
-
-
-//     const options = req.body;
-
-//     try {
-//         const response = await razorpay.orders.create(options);
-//         // res.json({
-//         //     // order_id:response.id,
-//         //     // currency:response.currency,
-//         //     // amount:response.amount
-
-//         // })
-//         console.log(response);
-//         res.json(response);
-//     } catch (error) {
-//         res.status(500).json({ message: "Error creating order" })
-//     }
-// })
-
-
-// app.get("/payment/:paymentId", async (req, res) => {
-//     const { paymentId } = req.params;
-//     const razorpay = new Razorpay({ key_id: process.env.RAZORPAY_KEY_ID, key_secret: process.env.RAZORPAY_KEY_SECRET });
-
-
-//     try {
-//         const payment = await razorpay.payments.fetch(paymentId);
-
-//         if (!payment) {
-//             return res.status(404).json({ message: "Payment not found" });
-//         }
-//         res.json({
-//             status: payment.status,
-//             method: payment.method,
-//             amount: payment.amount,
-//             currency: payment.currency,
-//         })
-
-
-//     } catch (error) {
-//         res.json({ message: "Error fetching payment" })
-//     }
-
-// })
-
-// app.post("/order/validate", async (req, res) => {
-//     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
-//     const sha = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
-//     sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
-//     const digest = sha.digest("hex");
-//     if (digest !== razorpay_signature) {
-//         return res.status(400).json({ message: "Invalid signature" });
-//     }
-//     res.json({
-//         message: "Payment successful",
-//         orderId: razorpay_order_id,
-//         paymentId: razorpay_payment_id
-//     })
-// })
 
 
 // ----------------xxxxxxxxxx----------------
