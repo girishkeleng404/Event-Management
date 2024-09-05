d68a36faf254993991220b209537f930f9a873a3
Girish keleng <girishkeleng30@gmail.com>
2024-09-05 19:56:31 +0530

    refactor: Move orderValidate route and controller to RazorpayController.js

--- api/controllers/RazorpayController.js
+++ api/controllers/RazorpayController.js
@@ -49,4 +49,19 @@ const paymentByPaymentId = async (req,res)=>{
 }
 
 
-export {orders, paymentByPaymentId};
+const orderValidate = async(req,res)=>{
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+    const sha = crypto.createHmac("sha256", process.env.RAZORPAY_KEY_SECRET);
+    sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+    const digest = sha.digest("hex");
+    if (digest !== razorpay_signature) {
+        return res.status(400).json({ message: "Invalid signature" });
+    }
+    res.json({
+        message: "Payment successful",
+        orderId: razorpay_order_id,
+        paymentId: razorpay_payment_id
+    })
+}
+
+export {orders, paymentByPaymentId, orderValidate};
--- api/routes/RazorpayRoute.js
+++ api/routes/RazorpayRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { orders, paymentByPaymentId } from '../controllers/RazorpayController.js';
+import { orders, orderValidate, paymentByPaymentId } from '../controllers/RazorpayController.js';
 
 const router = express.Router();
 
 router.route('/orders').post(orders);
 router.route('/payment/:paymentId').get(paymentByPaymentId);
+router.route('/order/validate').post(orderValidate);
 
 export default router;
