313816ef59766ecf03415f4a812cea4b8ab95af0
Girish keleng <girishkeleng30@gmail.com>
2024-09-09 20:24:05 +0530

    Refactor: Add client components.json file
    This commit adds a new file, , which includes configuration settings for the client components. The file specifies the schema, style, and other properties related to the components. This addition will help in organizing and managing the client components effectively.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,5 +19,10 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   
 })
