b24e32271a2d73a8c7a7b818569183d519504e47
Girish keleng <girishkeleng30@gmail.com>
2024-09-09 21:14:51 +0530

    Refactor: Update index.html and BookingInput component
    - Update index.html to add a script tag for the main.jsx file
    - Update BookingInput component to use the new Input component and add labels for name and guests fields
    - Remove commented out code for email input field
    - Update the label and placeholder for the email input field
    - Add label for the textarea field in BookingInput component
    -Update the Textarea component in Booki ngInput component to use the new textarea element

--- client/src/components/miniComponents/BookingInput.jsx
+++ client/src/components/miniComponents/BookingInput.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react"
 import { UserContext } from "../../UserContex"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
+import { Input } from "@/components/ui/input"
 
 export default function BookingInput({ name, setName, email, setEmail, guests, setGuests, phone, setPhone, note, setNote }) {
 
@@ -12,38 +13,37 @@ export default function BookingInput({ name, setName, email, setEmail, guests, s
         <div className="flex flex-col gap-6 lg:gap-16">
 
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-                <div className="flex items-center gap-2 ">
-                    <label className=" lg:text-xl">Name: </label>
-                    <input className="capitalize h-7 lg:h-8" type="text" value={name} onChange={ev => setName(ev.target.value)} />
+
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="email">Name:</Label>
+                    <Input type="text" id="name" placeholder="" value={name} onChange={ev => setName(ev.target.value)} />
                 </div>
-                <div className="flex items-center gap-2">
-                    <label className="lg:text-xl">Guests </label>
-                    <input type="number" className="h-7 lg:h-8" value={guests} onChange={ev => { setGuests(ev.target.value) }} />
+
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="email">Guests</Label>
+                    <Input type="number" id="guets" placeholder="" value={guests} onChange={ev => { setGuests(ev.target.value) }} />
                 </div>
-                <div className="flex items-center gap-2">
+
+
+                {/* <div className="flex items-center gap-2">
                     <label className="lg:text-xl">email:</label>
-                    <input type="email" className="h-7 lg:h-8" value={email} onChange={ev => { setEmail(ev.target.value) }} />
+                    <input type="email" className="h-7 lg:h-8"  />
+                </div> */}
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                    <Label htmlFor="email">Email</Label>
+                    <Input type="email" id="email" placeholder="" value={email} onChange={ev => { setEmail(ev.target.value) }}/>
                 </div>
+
+
                 <div className="flex items-center gap-2">
                     <label className="lg:text-xl">Phone: </label>
                     <input type="text" className="h-7 lg:h-8" value={phone} onChange={ev => { setPhone(ev.target.value) }} />
                 </div>
             </div>
 
-            {/* <div className="flex items-center gap-2">
-                <label className="lg:text-xl">Preference:</label>
-                <textarea
-                    name="Preference"
-                    id="Preference"
-                    rows=""  // Defines the number of visible rows
-                    value={note}
-                    onChange={(ev) => setNote(ev.target.value)}
-                    className="border rounded p-2 h-24 lg:h-32"
-                ></textarea>
-            </div> */}
             <div className="grid w-full gap-1.5">
                 <Label htmlFor="message-2">Your Preferences</Label>
-                <Textarea placeholder="Type your message here." id="message-2" value={note}  onChange={(ev) => setNote(ev.target.value)}  />
+                <Textarea placeholder="Type your message here." id="message-2" value={note} onChange={(ev) => setNote(ev.target.value)} />
                 <p className="text-sm text-muted-foreground">
                     Your message will be copied to the host.
                 </p>
